de42e77403d396e0eb02328af18ba34e1bf9638d
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-02-09 17:24:33 +0900

    Scaffolding frontend parts

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import "./bootstrap";
 import Vue from "vue";
 import App from "./App.vue";
 
+import "normalize.css";
+
 new Vue({
   el: "#app",
   render: h => h(App)
--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Vue - Laravel</title>
+  <title>Sunghwas</title>
 </head>
 <body>
   <div id="app"></div>
