411bd3bf2169b42518295403dcfe9131bf85c7f4
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-02-09 16:49:48 +0900

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+Mix.listen('configReady', (webpackConfig) => {
+  if (Mix.isUsing('hmr')) {
+    // Remove leading '/' from entry keys
+    webpackConfig.entry = Object.keys(webpackConfig.entry).reduce((entries, entry) => {
+    entries[entry.replace(/^\//, '')] = webpackConfig.entry[entry];
+      return entries;
+    }, {});
+    
+    // Remove leading '/' from ExtractTextPlugin instances
+    webpackConfig.plugins.forEach((plugin) => {
+      if (plugin.constructor.name === 'ExtractTextPlugin') {
+        plugin.filename = plugin.filename.replace(/^\//, '');
+      }
+    });
+  }
+});
